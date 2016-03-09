b255f7d3fdb560703831c9f37f4b6ea3625e99d8
AsimIkram <asimroots@hotmail.com>
2016-03-09 22:12:55 +0500

    Caclulator commit 1

---
+++ Calculator.go
@@ -0,0 +1,21 @@
+package Calculator
+
+//Add adds two numbers
+func Add(x float32, y float32) float32 {
+	return x + y
+}
+
+//Subtract subtracts two numbers
+func Subtract(x float32, y float32) float32 {
+	return x - y
+}
+
+//Multiply multiplies two numbers
+func Multiply(x float32, y float32) float32 {
+	return x * y
+}
+
+//Divide divides two numbers
+func Divide(x float32, y float32) float32 {
+	return x / y
+}
